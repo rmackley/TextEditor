7bc2b3881bf1b71172f12f5c78bbee8b9ac3baec
Rebecca Mackley <towersofbooks@gmail.com>
2020-02-16 22:13:34 -0500

    Added second text area

--- editor.py
+++ editor.py
@@ -8,6 +8,7 @@ TODO:
 - encrypt
 - config file for settings?
 - add keybinding for close?
+- second text area - does this have focus? to check which area to open file in?
 """
 
 class MainApp(tk.Frame):
@@ -18,11 +19,12 @@ class MainApp(tk.Frame):
         # Main Frame settings
         self.parent.title("Text Editor")
         self.parent.rowconfigure(0, minsize=800, weight=1)
-        self.parent.columnconfigure(1, minsize=800, weight=1)
+        self.parent.columnconfigure(1, minsize=700, weight=1)
         # self.parent.attributes("-fullscreen", True)
 
         #Components
         self.txt_area = ScrolledText.ScrolledText()
+        self.second_txt_area = ScrolledText.ScrolledText()
         self.fr_buttons = tk.Frame(self.parent, width="100")
         self.btn_open = tk.Button(self.fr_buttons, text="Open", command=self.open_file)
         self.btn_save = tk.Button(self.fr_buttons, text="Save As...", command=self.save_file)
@@ -30,6 +32,7 @@ class MainApp(tk.Frame):
         self.btn_fullscreen = tk.Button(self.fr_buttons, text="Fullscreen", command=self.toggle_fullscreen)
         self.btn_close = tk.Button(self.fr_buttons, text="Close", command=self.parent.destroy)
         self.btn_hide = tk.Button(self.fr_buttons, text="Hide Menu", command=self.close_menu)
+        self.btn_second_txt = tk.Button(self.fr_buttons, text="Second text area", command=self.open_second_txt)
         self.collapse_menu = tk.Frame(self.parent)
         self.menu_icon = tk.PhotoImage(file="menu.png")
         self.btn_show_menu = tk.Button(self.collapse_menu, image=self.menu_icon, text="Show menu", command=self.open_menu)
@@ -48,8 +51,10 @@ class MainApp(tk.Frame):
         self.btn_fullscreen.grid(row=4, column=0, sticky='ew', padx=5, pady=5)
         self.btn_close.grid(row=5, column=0, sticky='ew', padx=5, pady=5)
         self.btn_hide.grid(row=6, column=0, sticky='ew', padx=5, pady=5)
+        self.btn_second_txt.grid(row=7, column=0, sticky='ew', padx=5, pady=5)
 
     def open_file(self):
+        # print(self.parent.focus_get()) # returns '.!frame or .!frame2'
         """Open a file for editing."""
         self.filepath = askopenfilename(
             filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
@@ -97,6 +102,11 @@ class MainApp(tk.Frame):
     def open_menu(self):
         self.collapse_menu.grid_forget()
         self.fr_buttons.grid(row=0, column=0, sticky='ns')
+
+    def open_second_txt(self):
+        self.parent.columnconfigure(1, minsize=500, weight=1)
+        self.parent.columnconfigure(2, minsize=500, weight=1)
+        self.second_txt_area.grid(row=0, column=2, sticky='nsew')
 
 if __name__ == "__main__":
     root = tk.Tk()
